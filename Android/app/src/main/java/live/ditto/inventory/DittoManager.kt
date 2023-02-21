ae1f8edc00b31238397c83506171c4f8b81a0293
Shunsuke Kondo <shunsuke@ditto.live>
2023-02-21 17:04:52 +0900

    Initial commit for Android Inventory

---
+++ Android/app/src/main/java/live/ditto/inventory/DittoManager.kt
@@ -0,0 +1,124 @@
+package live.ditto.inventory
+
+import android.content.Context
+import android.util.Log
+import live.ditto.*
+import live.ditto.android.DefaultAndroidDittoDependencies
+
+object DittoManager {
+
+
+    /* Interfaces */
+    interface ItemUpdateListener {
+        fun setInitial(items: List<ItemModel>)
+        fun updateCount(index: Int, count: Int)
+    }
+
+
+    /* Settable from outside */
+    lateinit var itemUpdateListener: ItemUpdateListener
+
+
+    /* Get-only properties */
+    var ditto: Ditto? = null; private set
+
+
+    /* Private properties */
+    private const val COLLECTION_NAME = "inventories"
+    private var collection: DittoCollection? = null
+
+    private var subscription: DittoSubscription? = null
+    private var liveQuery: DittoLiveQuery? = null
+
+    private const val APP_ID = ""
+    private const val OFFLINE_LICENSE = ""
+
+
+    /* Internal functions and properties */
+    internal fun startDitto(context: Context) {
+
+        DittoLogger.minimumLogLevel = DittoLogLevel.VERBOSE
+        val dependencies = DefaultAndroidDittoDependencies(context)
+
+        try {
+            ditto = Ditto(dependencies, DittoIdentity.OfflinePlayground(dependencies, APP_ID))
+            ditto?.setOfflineOnlyLicenseToken(OFFLINE_LICENSE)
+            ditto?.startSync()
+        } catch (e: Exception) {
+            Log.e(e.message, e.localizedMessage)
+        }
+
+        collection = ditto?.store?.collection(COLLECTION_NAME)
+
+        observeItems()
+        insertDefaultDataIfAbsent()
+    }
+
+    internal fun increment(itemId: Int) {
+        collection?.findById(itemId)?.update {
+            it?.get("counter")?.counter?.increment(1.0)
+        }
+    }
+
+    internal fun decrement(itemId: Int) {
+        collection?.findById(itemId)?.update {
+            it?.get("counter")?.counter?.increment(-1.0)
+        }
+    }
+
+    internal val sdkVersion: String?
+        get() = ditto?.sdkVersion
+
+
+    /* Private functions and properties */
+
+    private fun insertDefaultDataIfAbsent() {
+
+        ditto?.store?.write { transaction ->
+            val scope = transaction.scoped(COLLECTION_NAME)
+
+            for (viewItem in itemsForView) {
+                val doc = collection?.findById(viewItem.itemId)?.exec()
+
+                if (doc == null) {
+                    scope.upsert(mapOf("_id" to viewItem.itemId, "counter" to DittoCounter()), writeStrategy = DittoWriteStrategy.InsertDefaultIfAbsent)
+                } else {
+                    viewItem.count = doc["counter"].intValue
+                }
+            }
+        }
+    }
+
+    private fun observeItems() {
+        val query = collection?.findAll()
+
+        subscription = query?.subscribe()
+
+        liveQuery = query?.observeLocal { docs, event ->
+
+            when (event) {
+
+                is DittoLiveQueryEvent.Initial -> {
+                    itemUpdateListener.setInitial(itemsForView.toMutableList())
+                }
+
+                is DittoLiveQueryEvent.Update -> {
+                    event.updates.forEach { index ->
+                        val doc = docs[index]
+                        val count = doc["counter"].intValue
+
+                        itemUpdateListener.updateCount(index, count)
+                    }
+                }
+            }
+        }
+    }
+
+    private val itemsForView = arrayOf(
+        ItemModel(0, R.drawable.coke, "Coca-Cola", 2.50, "A Can of Coca-Cola"),
+        ItemModel(1, R.drawable.drpepper, "Dr. Pepper", 2.50, "A Can of Dr. Pepper"),
+        ItemModel(2,R.drawable.lays, "Lay's Classic", 3.99, "Original Classic Lay's Bag of Chips"),
+        ItemModel(3, R.drawable.brownies, "Brownies", 6.50,"Brownies, Diet Sugar Free Version"),
+        ItemModel(4, R.drawable.blt, "Classic BLT Egg", 2.50, "Contains Egg, Meats and Dairy")
+    )
+}
---
+++ Android/app/src/main/java/live/ditto/inventory/DittoSDKInfoActivity.kt
@@ -0,0 +1,46 @@
+package live.ditto.inventory
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
+
+class DittoSDKInfoActivity : AppCompatActivity() {
+
+    companion object {
+        fun createIntent(context: Context, sdkInfo: String): Intent {
+            return Intent(context, DittoSDKInfoActivity::class.java).apply {
+                putExtra("sdkInfo", sdkInfo)
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ditto_sdk_info)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        title = "Ditto SDK Info"
+
+        val textView = findViewById<TextView>(R.id.ditto_sdk_info_text_view)
+
+        intent.getStringExtra("sdkInfo")?.let { sdkInfo ->
+            val platform = sdkInfo?.take(3) ?: "Unknown"
+            val versions = sdkInfo?.drop(3)?.split("_")
+            val semVer = versions[0]
+            val commitHash = versions[1]
+
+            textView.text = """
+                Platform: $platform
+                Version: $semVer
+                Commit Hash: $commitHash
+            """.trimIndent()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        onBackPressed()
+        return true
+    }
+}
